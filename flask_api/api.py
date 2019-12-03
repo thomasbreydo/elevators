b020897d3eb09ed3fdbd8534d4c28028bf0cb5c9
Thomas Breydo <student@macadmicbook104.dalton.org>
2019-12-03 07:50:12 -0500

    create api file

--- flask_api/api.py
+++ flask_api/api.py
@@ -1,4 +1,4 @@
-#!flask/bin/python
+#!/usr/bin/env python3
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,5 +10,4 @@ def index():
 
 
 if __name__ == '__main__':
-
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
